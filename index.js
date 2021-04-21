cb8fc99f4a60d76fcb87e39e38d91ae8ca5acacb
rajanjamu <rajanjamu@gmail.com>
2021-04-21 17:25:59 +0530

    slight modification

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const firebase = require("firebase");
+const express = require('express');
+const firebase = require('firebase');
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 firebase.initializeApp({
-  apiKey: "Om5VROO5S5B3kpGh1ywihjrv4V404nl7AdL2FgLv",
-  authDomain: "esp-32-9e307.firebaseapp.com",
-  databaseURL: "https://esp-32-9e307-default-rtdb.firebaseio.com",
-  storageBucket: "bucket.appspot.com",
+  apiKey: 'Om5VROO5S5B3kpGh1ywihjrv4V404nl7AdL2FgLv',
+  authDomain: 'esp-32-9e307.firebaseapp.com',
+  databaseURL: 'https://esp-32-9e307-default-rtdb.firebaseio.com',
+  storageBucket: 'bucket.appspot.com',
 });
 
 // Get a reference to the database service
 let database = firebase.database();
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.post("/ledOn", (req, res) => {
-  database.ref("/").set({ ledStatus: 1 }, function (error) {
+app.post('/ledOn', (req, res) => {
+  database.ref('/').set({ ledStatus: 1 }, function (error) {
     if (error) {
-      console.log("Failed with error: " + error);
+      console.log('Failed with error: ' + error);
     } else {
-      console.log("success");
+      console.log('success');
     }
   });
-  res.render("index");
+  res.redirect('/');
 });
 
-app.post("/ledOff", (req, res) => {
-  database.ref("/").set({ ledStatus: 0 }, function (error) {
+app.post('/ledOff', (req, res) => {
+  database.ref('/').set({ ledStatus: 0 }, function (error) {
     if (error) {
-      console.log("Failed with error: " + error);
+      console.log('Failed with error: ' + error);
     } else {
-      console.log("success");
+      console.log('success');
     }
   });
-  res.render("index");
+  res.redirect('/');
 });
 
 app.listen(port, () => {
